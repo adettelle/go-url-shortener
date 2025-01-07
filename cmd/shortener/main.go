8ea16ccc727da8b920f74476ad70ed36fa4a651f
Liudmila <l.litvinski@gmail.com>
2025-01-07 21:35:50 +0300

    add router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,60 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func PostMainPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	_, err := io.ReadAll(r.Body) // body
+	if err != nil {
+		http.Error(w, "Error in body", http.StatusBadRequest) // StatusInternalServerError
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("http://localhost:8080/EwHXdJfB")) // http://localhost:8080/EwHXdJfB
+	// w.Write([]byte("Hello"))
+
+}
+
+func GetID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	id := r.PathValue("id")
+	if id == "EwHXdJfB" {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Write([]byte("Location: https://practicum.yandex.ru/"))
+	} else {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.Write([]byte("Location: https://practicum.yandex.ru/")) // ???
+	}
+
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	r := http.NewServeMux() // // создаем сервер
+	r.HandleFunc("POST /", PostMainPage)
+	r.HandleFunc("GET /{id}", GetID)
+	port := ":8080"
+	fmt.Printf("Starting server on port %s\n", port)
+	return (http.ListenAndServe(port, r))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adettelle/go-url-shortner
+
+go 1.23.4
