b8865b25e0bf090480d7ba461fb53ed3002e832b
Liudmila <l.litvinski@gmail.com>
2025-01-13 17:54:33 +0300

    add tests

--- internal/storage/pathrepository.go
+++ internal/storage/pathrepository.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"fmt"
 	"time"
 
 	"golang.org/x/exp/rand"
@@ -15,30 +16,86 @@ func New() *PathStorage {
 	return &PathStorage{Paths: paths}
 }
 
-func (p *PathStorage) GetPath(name string) string {
-	return p.Paths[name]
+type NoEntryError struct {
+	name string
 }
 
-func (p *PathStorage) AddPath(fullPath string) string {
-	rangeStart := 5
+func (e *NoEntryError) Error() string {
+	return fmt.Sprintf("No Entry for name %s", e.name)
+}
+
+func (p *PathStorage) GetPath(name string) (string, error) {
+	if p.Paths[name] == "" {
+		return "", &NoEntryError{
+			name: name,
+		}
+	}
+	return p.Paths[name], nil
+}
+
+type EmptyPathError struct{}
+
+func (e *EmptyPathError) Error() string {
+	return "Empty full path!"
+}
+
+func (p *PathStorage) AddPath(fullPath string) (string, error) {
+	if fullPath == "" {
+		return "", &EmptyPathError{}
+	}
+	rangeStart := 2
 	rangeEnd := 10
 	offset := rangeEnd - rangeStart
 	randLength := seededRand.Intn(offset) + rangeStart
 
 	charSet := "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
-	randString := stringWithCharset(randLength, charSet)
+	randString, err := stringWithCharset(randLength, charSet)
+	if err != nil {
+		return "", err
+	}
 
 	p.Paths[randString] = fullPath
 
-	return randString
+	return randString, nil
+}
+
+type InvalidLengthError struct{}
+
+func (e *InvalidLengthError) Error() string {
+	return "Invalid length!"
+}
+
+type InvalidCharSetError struct{}
+
+func (e *InvalidCharSetError) Error() string {
+	return "Invalid char set!"
 }
 
 var seededRand = rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
 
-func stringWithCharset(length int, charset string) string {
+// stringWithCharset generates a random string of a specified length using the provided character set.
+// Parameters:
+//   - length: The desired length of the generated string.
+//   - charset: A string containing the characters to use for generating the random string.
+//
+// Returns:
+//   - A random string composed of characters from the given charset.
+//
+// Notes:
+//   - The function uses a seeded random number generator to ensure randomness.
+//   - If the charset is empty or the length is non-positive, the behavior is undefined. TODO!!!!!!!!!!!!!!!!!
+func stringWithCharset(length int, charset string) (string, error) {
+	if length <= 0 {
+		return "", &InvalidLengthError{}
+	}
+
+	if charset == "" {
+		return "", &InvalidCharSetError{}
+	}
+
 	b := make([]byte, length)
 	for i := range b {
 		b[i] = charset[seededRand.Intn(len(charset)-1)] // letterRunes[rand.Intn(len(letterRunes))]
 	}
-	return string(b)
+	return string(b), nil
 }
