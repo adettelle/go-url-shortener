ecd84b36a1fef7ab65b4b491619e7c80823c7038
Liudmila <l.litvinski@gmail.com>
2025-02-12 17:26:27 +0300

    change status code

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -273,5 +273,5 @@ func (h *Handlers) PostBatch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 }
